08e8bb4f31669ce50a875aee4c0810d421533443
lushdog <lushdog@3705b10a-cc46-4dfe-84f8-365302c7c60d>
2011-07-27 04:40:18 +0000

    Added beginings of Youtube interface.
    git-svn-id: https://lulz.unfuddle.com/svn/lulz_iosvideoplayer@35 3705b10a-cc46-4dfe-84f8-365302c7c60d

---
+++ iOSVideoPlayer/iOSVideoPlayer/Youtube/YoutubeBridge.h
@@ -0,0 +1,14 @@
+//
+//  YoutubeBridge.h
+//  iOSVideoPlayer
+//
+//  Created by Matthew Moore on 11-07-26.
+//  Copyright 2011 Matt Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@interface YoutubeBridge : NSObject
+{
+    NSString *foo; 
+}
+@end
---
+++ iOSVideoPlayer/iOSVideoPlayer/Youtube/YoutubeBridge.m
@@ -0,0 +1,30 @@
+//
+//  YoutubeBridge.m
+//  iOSVideoPlayer
+//
+//  Created by Matthew Moore on 11-07-26.
+//  Copyright 2011 Matt Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "YoutubeBridge.h"
+
+#define kApiKey AI39si6AIfVaikIH6D3Yw_TX2rFNzPEsuMb1yvWefpPGghgLB4h2EciSsV1qPP_SntqkUfg9pAN3bpGrq8rUWpwF2dphr3EvKQ
+
+@implementation YoutubeBridge
+
+//TODO: implement NSXMLParser
+//      implement NSURLConnection for google api
+
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
